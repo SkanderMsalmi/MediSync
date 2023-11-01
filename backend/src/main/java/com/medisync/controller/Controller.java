8000d74fa2bcb9b110d360f81d5e7abf4d5cb7de
SkanderMsalmi <msalmi.skander@esprit.tn>
2023-11-01 14:38:57 +0100

    request added  & add crossorigin

--- backend/src/main/java/com/medisync/controller/Controller.java
+++ backend/src/main/java/com/medisync/controller/Controller.java
@@ -2,12 +2,10 @@ package com.medisync.controller;
 
 import com.medisync.interfaces.ServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/hospital")
 public class Controller {
 	
