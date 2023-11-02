016948cdd99ab680406e2a5f2b1667a47c155126
SkanderMsalmi <msalmi.skander@esprit.tn>
2023-11-02 02:57:38 +0100

    updated

--- frontend/src/components/departments/ShowAllDep.js
+++ frontend/src/components/departments/ShowAllDep.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from 'react';
+import React from "react";
 import {
   Box,
   Card,
@@ -11,71 +11,102 @@ import {
   TableCell,
   TableHead,
   CardHeader,
-  TableContainer
-} from '@material-ui/core';
-import { Icon } from '@iconify/react';
-import arrowIosForwardFill from '@iconify/icons-eva/arrow-ios-forward-fill';
-import { Link as RouterLink } from 'react-router-dom';
-import Scrollbar from '../Scrollbar';
+  TableContainer,
+} from "@material-ui/core";
+import { Icon } from "@iconify/react";
+import arrowIosForwardFill from "@iconify/icons-eva/arrow-ios-forward-fill";
+import { Link as RouterLink } from "react-router-dom";
+import Scrollbar from "../Scrollbar";
+import Label from "../Label";
 
 const ShowAllDep = ({ departments }) => (
   <Card>
     <CardHeader title="Departments" sx={{ mb: 3 }} />
     <Scrollbar>
-    <TableContainer sx={{ minWidth: 720 }}>
+      <TableContainer sx={{ minWidth: 720 }}>
         <Table>
-        <TableHead>
+          <TableHead>
             <TableRow>
-                <TableCell>Department ID</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Type</TableCell>
-                <TableCell>Equipment</TableCell>
-                <TableCell>Medical Staff</TableCell>
-                <TableCell>Patients</TableCell>
-                <TableCell>Floor Number</TableCell>
+              <TableCell>Department ID</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Equipment</TableCell>
+              <TableCell>Medical Staff</TableCell>
+              <TableCell>Patients</TableCell>
+              <TableCell>Floor Number</TableCell>
             </TableRow>
-        </TableHead>
-        <TableBody>
+          </TableHead>
+          <TableBody>
             {departments.map((row, i) => {
-                console.log(row);
-                return (
-                  <TableRow key={i}>
-                    <TableCell>{row.department.split('#')[1]}</TableCell>
-                    <TableCell>{row.departmentName}</TableCell>
-                    <TableCell>{row.departmentType.split('#')[1]}</TableCell>
-                    <TableCell>{row.equipmentList.split(',').map((e)=>{
-                      return e+"\n"
-                    })}</TableCell>
-                    <TableCell>{row.medicalStaffList.split(',').map((m)=>{
-                      return m+"\n"
-                    })}</TableCell>
-                    <TableCell>{row.patientList.split(',').map((m)=>{
-                      return m+"\n"
-                    })}</TableCell>
-                    <TableCell>{row.floorNumber.split('^^')[0]}</TableCell>
-                  </TableRow>
-                );
+              console.log(row);
+              return (
+                <TableRow key={i}>
+                  <TableCell>{row.department.split("#")[1]}</TableCell>
+                  <TableCell>{row.departmentName}</TableCell>
+                  <TableCell>{row.departmentType.split("#")[1]}</TableCell>
+
+                  <TableCell>
+                    {row.equipmentList
+                      ? row.equipmentList.split(",").map((e) => {
+                          return (
+                            <div style={{ marginBottom: "0.5rem" }}>
+                              <Label variant="ghost" color="warning">
+                                {e}
+                              </Label>
+                            </div>
+                          );
+                        })
+                      : "No Patient Yet"}
+                  </TableCell>
+                  <TableCell>
+                    {row.medicalStaffList
+                      ? row.medicalStaffList.split(",").map((e) => {
+                          return (
+                            <div style={{ marginBottom: "0.5rem" }}>
+                              <Label variant="ghost" color="info">
+                                {e}
+                              </Label>
+                            </div>
+                          );
+                        })
+                      : "No Patient Yet"}
+                  </TableCell>
+                  <TableCell>
+                    {row.patientList
+                      ? row.patientList.split(",").map((e) => {
+                          return (
+                            <div style={{ marginBottom: "0.5rem" }}>
+                              <Label variant="ghost" color="success">
+                                {e}
+                              </Label>
+                            </div>
+                          );
+                        })
+                      : "No Patient Yet"}
+                  </TableCell>
+                  <TableCell>{row.floorNumber.split("^^")[0]}</TableCell>
+                </TableRow>
+              );
             })}
-        </TableBody>
+          </TableBody>
         </Table>
-    </TableContainer>
+      </TableContainer>
     </Scrollbar>
 
-      <Divider />
+    <Divider />
 
-      <Box sx={{ p: 2, textAlign: 'right' }}>
-        <Button
-          to="#"
-          size="small"
-          color="inherit"
-          component={RouterLink}
-          endIcon={<Icon icon={arrowIosForwardFill} />}
-        >
-          View All
-        </Button>
-      </Box>
-    </Card>
-  );
-
+    <Box sx={{ p: 2, textAlign: "right" }}>
+      <Button
+        to="#"
+        size="small"
+        color="inherit"
+        component={RouterLink}
+        endIcon={<Icon icon={arrowIosForwardFill} />}
+      >
+        View All
+      </Button>
+    </Box>
+  </Card>
+);
 
 export default ShowAllDep;
